5ad37e77a12414c42272a0d5c006a92e5cb5e056
programmerjide <programmerolajide@gmail.com>
2021-01-10 09:47:13 +0100

    Jokes App project Initialization.

---
+++ src/main/java/com/devolajide/joke/JokeappApplication.java
@@ -0,0 +1,13 @@
+package com.devolajide.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeappApplication.class, args);
+	}
+
+}
